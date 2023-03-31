a8f055fdf79793fe5c3e9c3bda223fb674349a2f
Joao Victor Souto <jheibol1212@icloud.com>
2023-03-30 21:34:00 -0300

    criar home

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+    
       <h1>Projeto Super Duper Legal</h1>
 
       <p>Texto explicando para que serve o projeto super legal!</p>
