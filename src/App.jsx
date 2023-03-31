ce3f77e95dbc2b466523264a373ca81edf9e3f6e
Hercules Ferreira <olcaminote82019@gmail.com>
2023-03-30 21:37:49 -0300

    apagado erro linha 6

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-HomePage;
+
 
 import { HomePage } from "../src/HomePage";
 
