bb353220fbcc28afb3d582104468eb41f68a1342
Hercules Ferreira <olcaminote82019@gmail.com>
2023-03-30 21:32:15 -0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Routes, Route } from 'react-router-dom';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+HomePage;
 
+import { HomePage } from "../src/HomePage";
+
+import { AboutPage } from "../src/AboutPage";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <h1>Projeto Super Duper Legal</h1>
 
       <p>Texto explicando para que serve o projeto super legal!</p>
+
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="" element={<AboutPage />}></Route>
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
